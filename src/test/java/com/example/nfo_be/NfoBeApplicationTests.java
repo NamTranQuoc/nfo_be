33ca85e4b2dd44dfc4c7448ae3a7f86ca9ae54d3
NamTran <nam.tran@zamo.io>
2022-01-20 21:53:37 +0700

    init

---
+++ src/test/java/com/example/nfo_be/NfoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nfo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
