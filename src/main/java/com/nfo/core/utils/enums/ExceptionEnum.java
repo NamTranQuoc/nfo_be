a50d2cbae4f0cc9da304152f0c0c64edbbca6c24
NamTran <nam.tran@zamo.io>
2022-01-30 23:03:19 +0700

    a

--- src/main/java/com/nfo/core/utils/enums/ExceptionEnum.java
+++ src/main/java/com/nfo/core/utils/enums/ExceptionEnum.java
@@ -1,12 +1,12 @@
 package com.nfo.core.utils.enums;
 
 public class ExceptionEnum {
-    public static final String cannot_connect = "cannot_connect";
-    public static final String phone_number_used = "phone_number_used";
-    public static final String param_not_null = "param_not_null";
-    public static final String member_exist = "member_exist";
-    public static final String member_not_exist = "member_not_exist";
-    public static final String password_incorrect = "password_incorrect";
-    public static final String confirm_password_incorrect = "confirm_password_incorrect";
-    public static final String member_type_deny = "member_type_deny";
+    public static final String cannot_connect = "Không thể kết nối";
+    public static final String phone_number_used = "Số điện thoại đã được sử dụng";
+    public static final String param_not_null = "Dữ liệu không được để trống";
+    public static final String member_exist = "Người dùng đã tồn tại";
+    public static final String member_not_exist = "Người dùng không tồn tại";
+    public static final String password_incorrect = "Mật khẩu không đúng";
+    public static final String confirm_password_incorrect = "Xác nhận mật khẩu không đúng";
+    public static final String member_type_deny = "Không có quyền truy cập";
 }
